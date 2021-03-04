a8e4bf8699420002918c592a871b17f460580697
Aleksei Voitovskii <al.voitovskii@gmail.com>
2021-03-04 18:29:39 +0300

    feat: initial project files commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>
+          Small task with grandma jam and ReactJS
+        </h1>
+      </Fragment>
+    )
+  }
+}
